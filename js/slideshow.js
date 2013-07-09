046df13c9cd1ea4e3ede1ef9118a03485c968dfa
bizi <alexandr@bizikov.ru>
2013-07-09 20:26:14 +0400

    изменено: образцы договоров, кнопка и страница стоимости услуг

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,9 +1,8 @@
 all_images = new Array (
-    "img/slides/1.jpg",
-    "img/slides/2.jpg",
     "img/slides/3.jpg",
-    "img/slides/4.jpg",
+//    "img/slides/2.jpg",
     "img/slides/5.jpg",
+    "img/slides/4.jpg",
     "img/slides/6.jpg",
   //  "img/slides/7.jpg",
     "img/slides/8.jpg",
@@ -11,6 +10,7 @@ all_images = new Array (
   //  "img/slides/10.jpg",
   //  "img/slides/11.jpg",
   //  "img/slides/12.jpg",
+    "img/slides/1.jpg",
     "img/slides/13.jpg");
 var ImgNum = 0;
 var ImgLength = all_images.length - 1;
